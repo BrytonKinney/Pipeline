fe6567a2ee57f4fad900866aecba2d7e2b8d9376
Bryton Kinney <brytonkinney@gmail.com>
2019-09-15 18:05:32 -0500

    Adding backend + sln file

--- Pipeline.BackEnd/Program.cs
+++ Pipeline.BackEnd/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Pipeline.FrontEnd
+namespace Pipeline.BackEnd
 {
     public class Program
     {
@@ -19,7 +19,7 @@ namespace Pipeline.FrontEnd
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("https://localhost:5002");
+                .UseUrls("https://localhost:4999")
+                .UseStartup<Startup>();
     }
 }
